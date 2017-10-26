66cf152eb5cb714ad9b10f453cea5980b372fad4
mairod <dorianlods@gmail.com>
2017-10-26 23:23:48 +0200

    dist

--- src/javascript/utils/manifest.js
+++
@@ -1,9 +0,0 @@
-const Manifest = {
-
-    matcap1: "assets/matcap.jpg",
-    noiseMap: "assets/noiseTest.jpg",
-    background: "assets/background_1.png",
-    particle: "assets/particles.png",
-
-}
-export default Manifest
--- src/shaders/snow.frag
+++
@@ -1,9 +0,0 @@
-uniform sampler2D texture;
-uniform float     pixelRatio;
-varying vec4      vColor;
-
-void main()
-{
-    // gl_FragColor = vColor;
-    gl_FragColor = vColor * texture2D(texture,gl_PointCoord);
-}
